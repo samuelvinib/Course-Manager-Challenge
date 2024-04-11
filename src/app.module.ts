2e473d3b9977f78b41262bc4a19bed81a03c57c0
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-04-11 11:07:15 -0300

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// eslint-disable-next-line
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
